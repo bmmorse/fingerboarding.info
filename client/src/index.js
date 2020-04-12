a9d2f9a908b22246016938e6c5960548b90746c4
Brendan Morse <42715623+brendan-morse@users.noreply.github.com>
2020-04-11 23:25:07 -0400

    update analytics

--- client/src/index.js
+++ client/src/index.js
@@ -62,7 +62,7 @@ class App extends React.Component {
                   }}
                   className="loadButton"
                 >
-                  Load More
+                  MORE
                 </button>
               </div>
             </Route>
